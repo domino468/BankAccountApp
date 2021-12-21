75857939f070c5b3531051d6ec43363606a7adaf
domino468 <dominik.adam4538@gmail.com>
2021-12-21 15:54:34 +0100

    adding methods of withdrawing, transferring, and depositing

--- src/main/java/BankAccountApp.java
+++ src/main/java/BankAccountApp.java
@@ -6,9 +6,11 @@ public class BankAccountApp {
         Savings savingsAcc1 = new Savings("Dominik Adam","987654321",2500);
 
         savingsAcc1.showInfo();
-
         System.out.println("***********");
-
         checkingAcc1.showInfo();
+
+        savingsAcc1.deposit(5000);
+        savingsAcc1.withdraw(200);
+        savingsAcc1.transfer(" PKO BP",3000);
     }
 }
